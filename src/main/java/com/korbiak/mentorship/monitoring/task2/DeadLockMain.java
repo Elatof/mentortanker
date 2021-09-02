42cb8a9f659d5d7c78e2804302ae11f0e56ddd2d
elatof <new210372@gmail.com>
2021-09-02 22:44:00 +0300

    monitoring: task 2 done

---
+++ src/main/java/com/korbiak/mentorship/monitoring/task2/DeadLockMain.java
@@ -0,0 +1,9 @@
+package com.korbiak.mentorship.monitoring.task2;
+
+public class DeadLockMain {
+    private static final DeadLockExample deadLockExample = new DeadLockExample();
+
+    public static void main(String[] args) {
+        deadLockExample.execute();
+    }
+}

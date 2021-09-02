c028569e426b20788c6ed005da93a49f3988fe5c
elatof <new210372@gmail.com>
2021-09-02 20:01:18 +0300

    monitoring: task 1 done

---
+++ src/main/java/com/korbiak/mentorship/monitoring/task1/OutOfMemoryMain.java
@@ -0,0 +1,10 @@
+package com.korbiak.mentorship.monitoring.task1;
+
+public class OutOfMemoryMain {
+    private static final OutOfMemoryErrorExample outOfMemoryErrorExample =
+            new OutOfMemoryErrorExample();
+
+    public static void main(String[] args) {
+        outOfMemoryErrorExample.execute();
+    }
+}
